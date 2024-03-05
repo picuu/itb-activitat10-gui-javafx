b596ced877856075b120665227f823f6478e0400
picuu <carloscapoforcada@gmail.com>
2024-03-05 23:42:29 +0100

    add logic to validate the form

--- src/main/kotlin/p/itbactivitat10guijavafx/FormController.kt
+++ src/main/kotlin/p/itbactivitat10guijavafx/FormController.kt
@@ -1,10 +1,13 @@
 package p.itbactivitat10guijavafx
 
+import exceptions.InvalidPunctuationException
+import exceptions.MissingAnswersException
 import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.RadioButton
 import javafx.scene.control.ToggleGroup
+import javafx.scene.paint.Color
 import models.Question
 
 class FormController {
@@ -12,8 +15,8 @@ class FormController {
         Question("On t'agrada passar més el temps?", "Platja", "Muntanya", "Bosc", "Ciutat"),
         Question("T'agrada viatjar?", "Molt", "Un parell de vegades a l'any", "Com menys millor", "No m'agrada"),
         Question("T'agradan els espais oberts?", "Molt", "Depèn del lloc", "Preferesc espais tancats", "M'agobien molt"),
-        Question("Quin clima prefereixes?", "Mediterrani", "Tropical", "Nòrdic", "Desèrtic"),
-        Question("Què sols fer quan tens vacances?", "Visitar museus", "Anar d'excursió", "Anar de compres", "Descansar"),
+        Question("Quin clima prefereixes?", "Mediterrani", "Tropical", "Desèrtic", "Nòrdic"),
+        Question("Què sols fer quan tens vacances?", "Anar d'excursió", "Visitar museus", "Anar de compres", "Descansar"),
     )
 
     private var currentQuestion: Int = 1
@@ -36,6 +39,10 @@ class FormController {
     private lateinit var previous: Button
     @FXML
     private lateinit var next: Button
+    @FXML
+    private lateinit var resultInfo: Label
+    @FXML
+    private lateinit var errorMessage: Label
 
     @FXML
     private fun onPreviousQuestion() {
@@ -63,6 +70,61 @@ class FormController {
     private fun onSelectAnswer() {
         val selectedAnswer = this.qToggleGroup.selectedToggle as RadioButton
         this.selectedAnswers.set(this.currentQuestion, selectedAnswer)
+
+        val question = this.questions[this.currentQuestion-1]
+        val selectedAnswerIndex = selectedAnswer.id.substring(selectedAnswer.id.length-1).toInt()
+
+        println(selectedAnswerIndex)
+
+        when (selectedAnswerIndex) {
+            1 -> question.setPunctuation(10)
+            2 -> question.setPunctuation(20)
+            3 -> question.setPunctuation(30)
+            4 -> question.setPunctuation(40)
+        }
+
+        this.errorMessage.isVisible = false
+        this.resultInfo.isVisible = false
+    }
+
+    @FXML
+    private fun onCheckAnswers() {
+        try {
+            if (this.selectedAnswers.size < 5) throw MissingAnswersException()
+            this.errorMessage.isVisible = false
+
+            var totalPunctuation = 0
+            this.questions.forEach { totalPunctuation += it.getPunctuation() }
+
+            println(totalPunctuation)
+
+            val resultText: String
+            when (totalPunctuation) {
+                in 50..100 ->  resultText = "Menorca"
+                in 110..120 -> resultText = "València"
+                in 130..150 -> resultText = "Saragossa"
+                in 160..180 -> resultText = "Barcelona"
+                in 190..200 -> resultText = "quedar-te a casa"
+                else -> throw InvalidPunctuationException()
+            }
+
+            this.resultInfo.text = "Tens una puntuació de $totalPunctuation. Per això\n" +
+                    "el teu millor destí per aquest estiu és $resultText."
+            this.resultInfo.textFill = Color.GREEN
+            this.resultInfo.isVisible = true
+        } catch (e: MissingAnswersException) {
+            println(e.message)
+            this.errorMessage.text = "Completa totes les respostes\nantes de verificar el formulari!"
+            this.errorMessage.textFill = Color.RED
+            this.errorMessage.isVisible = true
+        } catch (e: InvalidPunctuationException) {
+            println(e.message)
+            this.errorMessage.text = "Hi ha hagut un error verificant la teva puntuació!"
+            this.errorMessage.textFill = Color.RED
+            this.errorMessage.isVisible = true
+        } catch (e: Exception) {
+                e.printStackTrace()
+        }
     }
 
     private fun updateValues() {
