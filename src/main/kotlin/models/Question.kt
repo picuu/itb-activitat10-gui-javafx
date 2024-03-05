34773f761617aa35abf2e2346e7d47f23c9c85ec
ulisescastell <ulisescastell6@gmail.com>
2024-03-05 21:45:47 +0100

    add getters and attributes

--- src/main/kotlin/models/Question.kt
+++ src/main/kotlin/models/Question.kt
@@ -1,21 +1,49 @@
 package models
 
 class Question {
-    private var question: String
+    private var text: String
     private var answer1: String
     private var answer2: String
     private var answer3: String
     private var answer4: String
     private var userAnswer: Int
+    private var puntsResposta1: Int
+    private var puntsResposta2: Int
+    private var puntsResposta3: Int
+    private var puntsResposta4: Int
 
 
-    constructor(question: String, answer1: String, answer2: String, answer3: String, answer4: String) {
-        this.question = question
+    constructor(text: String, answer1: String, answer2: String, answer3: String, answer4: String) {
+        this.text = text
         this.answer1 = answer1
         this.answer2 = answer2
         this.answer3 = answer3
         this.answer4 = answer4
         this.userAnswer = -1
+        this.puntsResposta1 = 10
+        this.puntsResposta2 = 20
+        this.puntsResposta3 = 30
+        this.puntsResposta4 = 40
+    }
+
+    fun getText(): String {
+        return this.text
+    }
+
+    fun getAnswer1(): String {
+        return this.answer1
+    }
+
+    fun getAnswer2(): String {
+        return this.answer2
+    }
+
+    fun getAnswer3(): String {
+        return this.answer3
+    }
+
+    fun getAnswer4(): String {
+        return this.answer4
     }
 
     fun setUserAnswer(userAnswer: Int) {
@@ -27,7 +55,7 @@ class Question {
     }
 
     override fun toString(): String {
-        return "Question: ${this.question}" +
+        return "Question: ${this.text}" +
                 "\nAnswer 1: ${this.answer1}" +
                 "\nAnswer 2: ${this.answer2}" +
                 "\nAnswer 3: ${this.answer3}" +
